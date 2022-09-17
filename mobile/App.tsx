092b2f43a90e991b56fb6df1c993d61ad2c7be3e
Luciano Coelho <coelho.luciano@icloud.com>
2022-09-17 13:35:44 -0300

    mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,30 @@
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black 
+} from '@expo-google-fonts/inter';
+
+import { Background } from './src/components/Background';
 
 export default function App() {
-  return (
-    <View>
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black    
+  });
 
-    </View>
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ?  } 
+    </Background>
   );
 }
