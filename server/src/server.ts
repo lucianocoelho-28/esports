bffb2069083eaacaa1675aac7a6a2685fb69092f
Luciano Coelho <coelho.luciano@icloud.com>
2022-09-17 20:16:14 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,17 @@ const app = express()
 
 // localhost:3333/ads
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: 'Anuncio 1' },
         { id: 2, name: 'Anuncio 2' },
@@ -13,6 +23,12 @@ app.get('/ads', (request, response) => {
         { id: 5, name: 'Anuncio 5' },
 
     ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+    
+    return response.json([])
 })
 
 app.listen(3333)
