7c87cee5ed846eeeea1c953032b103fda357bcb8
Luciano Coelho <coelho.luciano@icloud.com>
2022-09-20 20:47:55 -0300

    mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,32 +1,63 @@
-import { StatusBar } from 'react-native';
-import { 
-  useFonts, 
-  Inter_400Regular, 
-  Inter_600SemiBold, 
-  Inter_700Bold, 
-  Inter_900Black 
-} from '@expo-google-fonts/inter';
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 
 import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
-import { Background } from './src/components/Background';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
-    Inter_700Bold, 
-    Inter_900Black    
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
   });
 
   return (
     <Background>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />} 
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
